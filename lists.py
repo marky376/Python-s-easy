f406ff283a8dc3a6ee4fa301c991bcb673da112b
marky376 <mmutuamark@gmail.com>
2023-12-04 14:10:27 +0300

    A commit on  a practical python program

---
+++ lists.py
@@ -0,0 +1,14 @@
+#fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+#fruits.count('apple')
+
+#fruits.count('tangerine')
+
+#fruits.index('banana')
+
+#fruits.index('banana', 4) # Find next banana starting at position 4
+
+#fruits.reverse()
+import sys
+mark = ['pen', 'paper', 'bag' 'bag', 'tissue', 'pen']
+
+mark.count('bag')
