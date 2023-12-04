e8f5b038828100124e2282dd8dfcfa5355f3a7a3
marky376 <mmutuamark@gmail.com>
2023-12-04 12:38:14 +0300

    A commit on  a practical python program

--- fibo1.py
+++ fibo1.py
@@ -1,11 +1,17 @@
+#LEARNING MODULES AND HOW TO USE THEM IN DIFFERENT FORMS
 #import fibo
 #from fibo import * (Introduces an unknown set of names into the interpreter possibly hiding some things
 #some things that a programmer has already defined.
-from fibo import fib, fib2
+#from fibo import fib, fib2
 #fibo.fib(1000)
 #fibo.fib2(100)
 #fibo.__name__
+#import fibo as fib
+from fibo import fib as fibonacci
+fibonacci(5000)
 
-fib(5000)
+
+
+#fib.fib(5000)
 
 
