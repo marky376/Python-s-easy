9acd89df2a2d70d14702785c4451387a3699a4b8
marky376 <mmutuamark@gmail.com>
2023-12-04 12:33:05 +0300

    A commit on  a practical python program

--- fibo1.py
+++ fibo1.py
@@ -1,6 +1,11 @@
-import fibo
+#import fibo
+#from fibo import * (Introduces an unknown set of names into the interpreter possibly hiding some things
+#some things that a programmer has already defined.
+from fibo import fib, fib2
+#fibo.fib(1000)
+#fibo.fib2(100)
+#fibo.__name__
 
-fibo.fib(1000)
-fibo.fib2(100)
-fibo.__name__
+fib(5000)
+
 
