c679a3c1fc958dff4ee6e81d5929185114100228
marky376 <mmutuamark@gmail.com>
2024-01-01 09:02:24 +0300

    Pretty basic python here

--- 01P.py/for2.py
+++ 01P.py/for2.py
@@ -4,9 +4,11 @@
 amount = input("Please enter the amount: ")
 interest = input("Please enter the expected interest: ")
 amount = float(amount)
-interest = float(interest)
+interest = round(float(interest), 2)
 # Each year their investment will  increase by their investment + their investment * interest rate is
-new_interest = ((interest * amount) + amount)
+for i in range(10):
+    new_interest = ((interest * amount) + amount)
+    ++i
 # Print out the eranings after a 10 year period
 print("Your newly generated earnings is {:.2f}".format(new_interest))
 
