dab53cdd17c4d7625133a61b742ac3a8895c45fe
marky376 <mmutuamark@gmail.com>
2024-01-11 17:25:21 +0300

    Pretty much basic Python here

--- 01P.py/func3.py
+++ 01P.py/func3.py
@@ -8,9 +8,16 @@
 #will deal with addition
 
 # Receive the string and split the string into variables
+def solve_eq(equation):
+    x, add, num1, equal, num2 = equation.split()
+
+    num1, num2 = int(num1), int(num2)
+
 
 # Convert the strings into integers
 
 # Convert the result into a string and join them to the string "X = "
+    return "x = " + str(num2 - num1)
 
 # print()
+
