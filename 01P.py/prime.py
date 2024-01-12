d9e394edc65bca569067560c0b555401cddd138f
marky376 <mmutuamark@gmail.com>
2024-01-12 07:16:56 +0300

    Pretty much basic Python here

--- 01P.py/prime.py
+++ 01P.py/prime.py
@@ -9,7 +9,7 @@
 
 # Use a for loop and check if modulus == 0 True
 def is_prime(num):
-    for i in range(1, num):
+    for i in range(2, num):
         if (num % i == 0):
             return False
 
