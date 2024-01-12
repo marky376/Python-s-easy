5d95edda340bbf8e643792683dc2d7f84a55090c
marky376 <mmutuamark@gmail.com>
2024-01-12 08:07:55 +0300

    Pretty much basic Python here

--- 01P.py/using_main.py
+++ 01P.py/using_main.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
+import Pi from math
 def get_area(shape):
     shape = shape.lower()
 
     if shape == "rectangle":
+    
         rectangle_area()
     
+    
     elif shape == "circle":
+    
         circle_area()
+    
     elif shape == "triangle":
+    
         triangle_area()
+    
     else:
         print("Please enter rectangle, square or triangle")
 
 def rectangle_area():
     length = float(input("Please enter the length of the rectangle : "))
+    
     width = float(input("Please enter the width of the rectangle : "))
+    
     Area = length * width
+    
     print(f"The area of the rectangle with {length} as the length and {width} as the width is {Area}")
 
 def circle_area():
-    set pi as 3.14
+
     radius = float(input("Please enter the radius of the circle : "))
+
+    Area = radius * Pi * radius
 
 def main():
     shape_type = input("Get area for what shape? : ")
