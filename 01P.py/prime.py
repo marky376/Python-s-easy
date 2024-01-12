2c03bcd59748be135993cef9fb39ff23ae8c6e10
marky376 <mmutuamark@gmail.com>
2024-01-12 06:49:42 +0300

    Pretty much basic Python here

--- 01P.py/prime.py
+++ 01P.py/prime.py
@@ -2,3 +2,7 @@
 
 # A prime can only be divided by 1 and itself
 # 5 is a prime because it can only be divided by 1 and 5 = positive factor
+
+# 6 is not a prime because it can be divided by 1, 2, 3 and itself
+
+
